23ed3d5702f717522e9b0177cc30d595cc1d0682
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-02 16:39:12 -0700

    Update SpearHead_Library.py

--- SpearHead_Library.py
+++ SpearHead_Library.py
@@ -3,6 +3,7 @@ from llama_index import SimpleDirectoryReader
 from llama_index.node_parser import SimpleNodeParser
 from llama_index import VectorStoreIndex
 from pdf2image import convert_from_path
+import pytesseract
 import threading
 import io
 
@@ -17,24 +18,26 @@ def load_data_and_index(text_data):
     return index, query_engine
 
 def process_pdf_data(pdf_data_list):
-    text_results = []  # List to store processed text from each PDF
+    text_results = []
     for pdf_data in pdf_data_list:
-        # Process the PDF data here; convert to text using your preferred method
-        # For the current mockup, I'll just use a dummy conversion
-        text_data = "Dummy Text " + pdf_data[:20]  # Modify this with your actual conversion method
+        buffer = io.BytesIO(pdf_data)
+        pages = convert_from_path(buffer)
+        text_data = ""
+        for i, page in enumerate(pages):
+            image = page.convert('L')
+            text = pytesseract.image_to_string(image)
+            text_data += text + f'\nPage: {i + 1}\n'
         text_results.append(text_data)
     return text_results
 
 def spearhead_library():
     st.title("SpearHead Library")
 
-    # PDF upload
     uploaded_files = st.file_uploader("Upload PDFs", type="pdf", accept_multiple_files=True)
     process_button = st.button("Process PDFs")
     user_query = st.text_input("Enter your question:")
     status_box = st.empty()
 
-    # Initiate session state for PDF data and processed texts
     if "pdf_data" not in st.session_state:
         st.session_state.pdf_data = []
     if "text_data" not in st.session_state:
@@ -42,11 +45,9 @@ def spearhead_library():
 
     if process_button:
         if uploaded_files:
-            # Store the uploaded PDFs in session state
             for uploaded_file in uploaded_files:
                 st.session_state.pdf_data.append(uploaded_file.read())
             status_box.write("Processing PDFs...")
-            # Process the uploaded PDFs
             processed_texts = process_pdf_data(st.session_state.pdf_data)
             st.session_state.text_data.extend(processed_texts)
             status_box.write("PDFs processed and converted to text.")
@@ -63,5 +64,3 @@ def spearhead_library():
 
 if __name__ == "__main__":
     spearhead_library()
-
-
