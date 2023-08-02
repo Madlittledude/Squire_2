ae0bd458490b0a8c4fe69ae8ce11520b59560dc0
Madlittledude <64448401+Madlittledude@users.noreply.github.com>
2023-08-02 16:21:39 -0700

    Update SpearHead_Library.py

--- SpearHead_Library.py
+++ SpearHead_Library.py
@@ -1,31 +1,29 @@
 import streamlit as st
-import os
 from llama_index import SimpleDirectoryReader
 from llama_index.node_parser import SimpleNodeParser
 from llama_index import VectorStoreIndex
 from pdf2image import convert_from_path
 import threading
+import io
 
-def load_data_and_index(path_to_texts):
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def load_data_and_index(text_data):
     filename_fn = lambda filename: {'file_name': filename}
-    documents = SimpleDirectoryReader(path_to_texts, file_metadata=filename_fn).load_data()
+    documents = [{'content': text, 'metadata': filename_fn(f"text_{i}")} for i, text in enumerate(text_data)]
     parser = SimpleNodeParser()
     nodes = parser.get_nodes_from_documents(documents)
     index = VectorStoreIndex.from_documents(documents)
     query_engine = index.as_query_engine(max_nodes=6, max_tokens=500)
     return index, query_engine
 
-def save_images_from_pdf(pdf_path, image_path):
-    images = convert_from_path(pdf_path)
-    for i, img in enumerate(images):
-        img.save(f"{image_path}/page_{i}.png", "PNG")
-
-def process_pdfs(status_box, path_to_pdfs, path_to_texts):
-    for file in os.listdir(path_to_pdfs):
-        if file.endswith(".pdf"):
-            pdf_path = os.path.join(path_to_pdfs, file)
-            save_images_from_pdf(pdf_path, path_to_texts)
-    status_box.write("PDFs processed and converted to text.")
+def process_pdf_data(pdf_data_list):
+    text_results = []  # List to store processed text from each PDF
+    for pdf_data in pdf_data_list:
+        # Process the PDF data here; convert to text using your preferred method
+        # For the current mockup, I'll just use a dummy conversion
+        text_data = "Dummy Text " + pdf_data[:20]  # Modify this with your actual conversion method
+        text_results.append(text_data)
+    return text_results
 
 def spearhead_library():
     st.title("SpearHead Library")
@@ -36,25 +34,25 @@ def spearhead_library():
     user_query = st.text_input("Enter your question:")
     status_box = st.empty()
 
+    # Initiate session state for PDF data and processed texts
+    if "pdf_data" not in st.session_state:
+        st.session_state.pdf_data = []
+    if "text_data" not in st.session_state:
+        st.session_state.text_data = []
+
     if process_button:
         if uploaded_files:
-            # Create directories if they don't exist
-            if not os.path.exists("Library/PDF"):
-                os.makedirs("Library/PDF")
-            if not os.path.exists("Library/TEXT"):
-                os.makedirs("Library/TEXT")
-
-            # Save the PDFs
+            # Store the uploaded PDFs in session state
             for uploaded_file in uploaded_files:
-                with open(f"Library_PDF/{uploaded_file.name}", "wb") as f:
-                    f.write(uploaded_file.getbuffer())
-            
+                st.session_state.pdf_data.append(uploaded_file.read())
             status_box.write("Processing PDFs...")
-            # Process the uploaded PDFs in a separate thread to prevent blocking
-            threading.Thread(target=process_pdfs, args=(status_box, "Library/PDF", "Library/TEXT")).start()
-    
-    if user_query:
-        index, query_engine = load_data_and_index('Library/TEXT')
+            # Process the uploaded PDFs
+            processed_texts = process_pdf_data(st.session_state.pdf_data)
+            st.session_state.text_data.extend(processed_texts)
+            status_box.write("PDFs processed and converted to text.")
+
+    if user_query and st.session_state.text_data:
+        index, query_engine = load_data_and_index(st.session_state.text_data)
         response = query_engine.query(user_query)
         response_text = response.response
         filenames = [node.node.metadata['file_name'].split('/')[-1].split('.')[0] for node in response.source_nodes]
@@ -65,4 +63,5 @@ def spearhead_library():
 
 if __name__ == "__main__":
     spearhead_library()
+
 
